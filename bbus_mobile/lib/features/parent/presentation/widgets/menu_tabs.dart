4eb4b98e5a6962b6e6f16a60ed567c33f7d19c1c
ThaoTTP <dreemmurrchara@gmail.com>
2025-03-21 12:10:46 +0700

    pass props through bus status view

--- bbus_mobile/lib/features/parent/presentation/widgets/menu_tabs.dart
+++ bbus_mobile/lib/features/parent/presentation/widgets/menu_tabs.dart
@@ -4,17 +4,19 @@ import 'package:bbus_mobile/common/entities/bus.dart';
 import 'package:bbus_mobile/common/notifications/notification_service.dart';
 import 'package:bbus_mobile/config/injector/injector.dart';
 import 'package:bbus_mobile/config/theme/colors.dart';
+import 'package:bbus_mobile/core/errors/exceptions.dart';
+import 'package:bbus_mobile/core/utils/logger.dart';
 import 'package:bbus_mobile/features/map/cubit/location_tracking/location_tracking_cubit.dart';
+import 'package:bbus_mobile/features/parent/data/datasources/children_datasource.dart';
 import 'package:bbus_mobile/features/parent/domain/entities/daily_schedule.dart';
-import 'package:bbus_mobile/features/parent/domain/usecases/get_bus_detail.dart';
 import 'package:bbus_mobile/features/parent/presentation/widgets/bus_info_tab_view.dart';
 import 'package:bbus_mobile/features/parent/presentation/widgets/status_tab_view.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MenuTabs extends StatefulWidget {
-  const MenuTabs({super.key});
+  final String childId;
+  const MenuTabs({super.key, required this.childId});
 
   @override
   State<MenuTabs> createState() => _MenuTabsState();
@@ -22,28 +24,93 @@ class MenuTabs extends StatefulWidget {
 
 class _MenuTabsState extends State<MenuTabs> with TickerProviderStateMixin {
   TabController? _tabController;
-  StreamSubscription<RemoteMessage>? _notifSubscription;
+  StreamSubscription<dynamic>? _notifSubscription;
   DailyScheduleEntity? _trackingSchedule;
+  bool _isLoading = true;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _tabController = TabController(length: 2, vsync: this, initialIndex: 0);
-    _trackingSchedule = DailyScheduleEntity(
-      id: 2,
-      date: "2025-03-02",
-      pickup: EventDetail(time: null, address: "456 Oak Street, Springfield"),
-      attendance: EventDetail(time: null, address: "Trường tiểu học Ngôi Sao"),
-      drop: EventDetail(time: null, address: "456 Oak Street, Springfield"),
-    );
-    final _notifService = sl<NotificationService>();
-    _notifSubscription = _notifService.notificationStream.listen((message) {});
+    _init();
+  }
+
+  void _init() async {
+    try {
+      final res =
+          await sl<ChildrenDatasource>().getChildAttendance(widget.childId);
+      setState(() {
+        _trackingSchedule = DailyScheduleEntity(
+          pickup: EventDetail.withFormattedTime(
+              time: res[1].checkin, address: res[1].checkpointName),
+          attendance: EventDetail.withFormattedTime(
+              time: res[1].checkout,
+              timeLeave: res[0].checkin,
+              address: "Trường tiểu học Ngôi Sao"),
+          drop: EventDetail.withFormattedTime(
+              time: res[0].checkout, address: res[0].checkpointName),
+        );
+        _isLoading = false;
+      });
+      final _notifService = sl<NotificationService>();
+      _notifSubscription = _notifService.notificationStream.listen((message) {
+        logger.i('new message');
+        setState(() {
+          if (message['direction'] == 'PICK_UP' &&
+              message['status'] == 'IN_BUS') {
+            _trackingSchedule = _trackingSchedule!.copyWith(
+                pickup:
+                    _trackingSchedule!.pickup!.copyWith(time: message['time']));
+          } else if (message['direction'] == 'PICK_UP' &&
+              message['status'] == 'ATTENDED') {
+            _trackingSchedule = _trackingSchedule!.copyWith(
+                pickup: _trackingSchedule!.attendance!
+                    .copyWith(time: message['time']));
+          } else if (message['direction'] == 'DROP_OFF' &&
+              message['status'] == 'IN_BUS') {
+            _trackingSchedule = _trackingSchedule!.copyWith(
+                pickup: _trackingSchedule!.attendance!
+                    .copyWith(timeLeave: message['time']));
+          } else if (message['direction'] == 'DROP_OFF' &&
+              message['status'] == 'ATTENDED') {
+            _trackingSchedule = _trackingSchedule!.copyWith(
+                pickup:
+                    _trackingSchedule!.drop!.copyWith(time: message['time']));
+          }
+        });
+      });
+    } on EmptyException catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      logger.e(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Không có lịch chạy xe cho ngày hôm nay'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    } on ServerException catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      logger.e(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Lỗi không xác định'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    } catch (e) {
+      logger.e(e.toString());
+    }
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     _tabController?.dispose();
+    _notifSubscription?.cancel();
     super.dispose();
   }
 
@@ -94,6 +161,7 @@ class _MenuTabsState extends State<MenuTabs> with TickerProviderStateMixin {
                 children: [
                   StatusTabView(
                     trackingSchedule: _trackingSchedule,
+                    isLoading: _isLoading,
                   ),
                   BusInfoTabView(
                     busDetail: context.read<LocationTrackingCubit>().busDetail!,
